c31a39bb71da283f5e04324a3afc07ddea9d86e6
jarvis394 <tarnatovski@gmail.com>
2022-03-31 18:31:26 +0300

    Remove redux-logger from bundle

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,14 @@
 import { createStore, applyMiddleware, Middleware } from 'redux'
-import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import reducers from './reducers'
 
 const middlewares: Middleware[] = [thunkMiddleware]
 
 if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { createLogger } = require('redux-logger')
   middlewares.push(createLogger({ collapsed: true }))
 }
 
 export type RootState = ReturnType<typeof reducers>
-export default createStore(
-  reducers,
-  applyMiddleware(...middlewares)
-)
+export default createStore(reducers, applyMiddleware(...middlewares))
